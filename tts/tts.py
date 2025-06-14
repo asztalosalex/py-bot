a4854924d11b01d78e27703f2fe74f40e26a867c
Alex Asztalos <asztalos.alex93@gmail.com>
2025-06-14 15:13:00 +0200

    change model

--- tts/tts.py
+++ tts/tts.py
@@ -7,24 +7,23 @@ load_dotenv()
 
 class TTS:
 
-    VOICE_ID = "TumdjBNWanlT3ysvclWh"
-    MODEL_ID = "eleven_flash_v2_5"
+    VOICE_ID = "TxGEqnHWrfWFTfGW9XjX"
+    MODEL_ID = "eleven_turbo_v2_5"
+    API_KEY = os.getenv('ELEVENLABS_API_KEY')
     VOICE_SETTINGS = {
         "stability": 0.5, 
-        "similarity_boost": 0.8, 
-        "style": 0.0, 
+        "similarity_boost": 0.5, 
+        "style": 0.1, 
         "use_speaker_boost": True, 
         "speed": 0.9
     }
 
     def __init__(self):
-        self.eleven_labs = ElevenLabs(api_key=os.getenv('ELEVENLABS_API_KEY'))
-
-
+        self.eleven_labs = ElevenLabs(api_key=self.API_KEY)
 
 
     def generate_audio(self, text: str): 
-        audio_stream = self.eleven_labs.text_to_speech.convert_as_stream(
+        audio_stream = self.eleven_labs.text_to_speech.convert(
                         text=text,
                         voice_id=self.VOICE_ID, 
                         model_id=self.MODEL_ID,
