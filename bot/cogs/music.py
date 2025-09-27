243f07eb174e0f5716fef49369b6545ac43cecb3
Alex Asztalos <asztalos.alex93@gmail.com>
2025-09-27 20:25:17 +0200

    add browser cookies to the yt-dlp options

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -20,6 +20,8 @@ YDL_OPTS = {
     "quiet": True,
     "default_search": "ytsearch",
     "skip_download": True,
+    "cookiesfrombrowser": ("chrome",),
+    "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 }
 
 FFMPEG_OPTS = {
